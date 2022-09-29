4c3b8ee1bb9ea2c9155df682cfdabb2ca7b5d798
vishesh147 <vishesh.r.patel@gmail.com>
2022-09-30 02:00:12 +0530

    ifest22tmp

--- ifest22tmp/wsgi.py
+++ ifest22tmp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ifest22tmp project.
+WSGI config for iFest project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ifest22tmp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iFest.settings')
 
 application = get_wsgi_application()
